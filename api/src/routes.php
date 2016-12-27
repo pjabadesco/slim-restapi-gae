3fdbda43a0ab2ffdc4254560821ed077102d388d
Paolo Josef J. Abadesco <pjabadesco@gmail.com>
2016-12-27 21:01:28 +0800

    api

--- api/src/routes.php
+++ api/src/routes.php
@@ -10,15 +10,6 @@ $app->group('/hello', function () {
     $this->get('/method1', 'HelloController:method1');
     $this->get('/method2', 'HelloController:method2');
     $this->get('/method3', 'HelloController:method3');
-/*    $this->get   ('',             _Controller_oAuth2::class.':getAll');
-    $this->get   ('/{id:[0-9]+}', _Controller_oAuth2::class.':get');
-    $this->post  ('',             _Controller_oAuth2::class.':add');
-    $this->put   ('/{id:[0-9]+}', _Controller_oAuth2::class.':update');
-    $this->delete('/{id:[0-9]+}', _Controller_oAuth2::class.':delete');*/
-//})->add(function ($request, $response, $next) {
-//	$this->settings['localtable'] = "categories";
-//    $response = $next($request, $response);
-//    return $response;
 });
 
 $app->group('/users', function () {
